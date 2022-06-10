3e0b5205fc9cc82a4007c897cb3a499312070712
paco_el_flaco <paco_el_flaco@hotmail.com>
2022-06-09 19:11:22 -0500

    se implementaron funciones las cuales reciben argumentos para llenar los parametros correspondientes y claqcular la figura deseada

--- figurasgeo.js
+++ figurasgeo.js
@@ -2,42 +2,63 @@
 console.log("hola yeider")
 console.group("cuadrado")
 /*se creara una calculadora del perimetro y/o area del cuadrado*/
-const ladocuadrado = 5;
-console.log(`los lados del cuadrado miden ${ladocuadrado}`);
-const perimetrocuadrado = 4*ladocuadrado;
+
+function perimetrocuadrado(lado) {
+    var resultado = 4*lado;
+    console.log(`los lados del cuadrado miden ${lado}`);
+    console.log(`el perimetro del cuadrado es ${resultado}.cm`)
+    return resultado;
+};
 // aun no se como exponer un numero a una potencia asi que 
 //por ahora solo lo multiplicare por el mismo
-const areacuadrado = ladocuadrado * ladocuadrado;
-console.log(`el perimetro del cuadrado es ${perimetrocuadrado}.cm`)
-console.log(`el area del cuadrado es ${areacuadrado}.cm^2`)
-console.groupEnd()
+function areacuadrado(ladocuadrado) { 
+    var resultado =ladocuadrado * ladocuadrado;
+    console.log(`los lados del cuadrado miden ${ladocuadrado}`);
+    console.log(`el area del cuadrado es ${resultado}.cm^2`);
+    return resultado;
+};
+console.groupEnd();
 
 /* calcular el perimetro y area de un triangulo*/ 
-console.group("triangulo")
-const lado1 = 4;
-const lado2 = 5;
-const lado3 = 6;
-const basetriangulo = 6;
-const alturatriangulo = 10;
-const perimetrotriangulo = (lado1 + lado2 + lado3)
-const areatriangulo = ((basetriangulo * alturatriangulo)/2)
-console.log(`los lados del cuadrado son:
-lado1: ${lado1} lado2: ${lado2}, lado3 ${lado3}
-la altura es ${alturatriangulo} y la base es ${basetriangulo}`)
-console.log(`el area del triangulo es ${areatriangulo}cm
-el perimetro del triangulo es ${perimetrotriangulo}cm`)
+
+console.group("triangulo");
+function perimetrotriangulo(lado1,lado2,lado3) {
+    var resultado = (lado1 + lado2 + lado3); 
+    console.log(`los lados del triangulo son:
+lado1: ${lado1} lado2: ${lado2}, lado3 ${lado3}`);
+console.log(`el perimetro del triangulo es ${resultado}cm`);
+    return resultado;
+}
+function areatriangulo (basetriangulo,alturatriangulo){
+    var resultado = (basetriangulo * alturatriangulo)/2;
+    console.log(`el area del triangulo es ${resultado}cm`);
+    return resultado;
+}
 console.groupEnd();
 
 /*calular el perimetro y area de un circulo */
-console.group("circulo")
-const diametrocirculo= 10;
-const radiocirculo = 7;
-const pi = Math.PI;
-const perimetrocirculo = diametrocirculo * pi;
-const areacirculo = (pi * (radiocirculo*radiocirculo));
-console.log(`el diametro dl circulo es ${diametrocirculo}cm
-el radio del circulo es ${radiocirculo}
-y pi vale ${pi}`)
-console.log(`el perimetro del circulo es ${perimetrocirculo}cm
-el area del circulo ${areacirculo}cm`);
+
+console.group("circulo");
+function diametrocirculo (radiocirculo){
+    const resultado = radiocirculo * 2;
+    console.log(`el diametro del circulo es ${resultado}cm
+el radio del circulo es ${radiocirculo}`);
+    return resultado;
+};
+
+function perimetrocirculo (radiocirculo){
+    const pi = Math.PI;
+    const diametro = diametrocirculo(radiocirculo);
+    const resultado = diametro*pi
+    console.log(`el perimetro del circulo es ${resultado}cm`);
+    return resultado;
+};
+
+function areacirculo(radiocirculo){
+    const pi = Math.PI;
+    const resultado = pi*(radiocirculo*radiocirculo);
+    console.log(`el area del circulo ${resultado}cm`);
+    console.log(`pi vale ${pi}`);
+    return resultado;
+};
 console.groupEnd()
